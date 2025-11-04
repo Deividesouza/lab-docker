4960e2393b49d6068da3656d8b79b12a28fb50d0
Deividesouza <deivide111@gmail.com>
2025-11-03 23:38:14 -0300

    feat(pipeline): adiciona build automatizado com Makefile e Kaniko

---
+++ lab4-makefiles/script.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+echo "Hello, World! This is a script running inside a Docker container."
+echo "Olá, $1! Este container foi buildado com Kaniko e executado com sucesso."
+echo "Este container foi criado usando Makefiles para automatizar o processo de build."
